f210a18e44eaf09fe3b44e76325ce769b48475f9
Virendra <virendra.thakur31@gmail.com>
2021-09-29 21:18:20 +0530

    User model added.

---
+++ CoreWebApplication1/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace CoreWebApplication1.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
