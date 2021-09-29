c6a5535152021bcc6f5a3998c2170455f3912bbb
Virendra <virendra.thakur31@gmail.com>
2021-09-29 21:15:11 +0530

    EFDataContext added.

---
+++ CoreWebApplication1/EFDataContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreWebApplication1
+{
+    public class EFDataContext
+    {
+    }
+}
